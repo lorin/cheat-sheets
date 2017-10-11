74d9173f0944ae601a0a6f883443ed65a10c99b1
Lorin Hochstein <lhochstein@netflix.com>
2017-10-11 16:29:34 -0700

    mysql

---
+++ mysql-cheat-sheet/mysql.rb
@@ -0,0 +1,90 @@
+cheatsheet do
+    title 'mysql'
+    docset_file_name 'mysql'
+    keyword 'mysql'
+    category do
+        id 'Basics'
+        entry do
+            name 'port'
+            command '3306'
+        end
+    end
+    category do
+        id 'Docker'
+        entry do
+            name 'Container info'
+            notes <<-'END'
+            https://hub.docker.com/_/mysql/
+            END
+        end
+        entry do
+            name 'Initialize a container'
+            notes <<-'END'
+            * Put .sh, .sql or .sql.gz in dir mounted at /docker-entrypoint-initdb.d
+
+            Then do:
+
+            ```
+            docker run -v /path/to/sql:/docker-entrypoint-initdb.d ...
+            ```
+
+            END
+        end
+        entry do
+            name 'Run inside a container'
+            notes <<-'END'
+            ```bash
+            docker run --name mysql -d -e MYSQL_ROOT_PASSWORD=password -p3306:3306 mysql:5.6
+            ```
+            END
+        end
+        entry do
+            name 'connect to container'
+            notes <<-'END'
+            If you use "localhost", mysql client defaults to unix domain sockets,
+            so you must use (assumes password is "password"):
+
+            ```bash
+            mysql -h 127.0.0.1 -u root -ppassword
+            ```
+
+            Alternately, you can specify --protocol:
+
+            ```bash
+            mysql --protocol tcp -u root -ppassword
+            ```
+
+            END
+        end
+    end
+    category do
+        id 'Dates and times'
+        entry do
+            name 'Set the session time zone'
+            notes <<-'END'
+            ```
+            SET time_zone=utc;
+            ```
+            END
+        end
+        entry do
+            name 'Check the current time zone setting'
+            notes <<-'END'
+            ```sql
+            SELECT @@global.time_zone, @@session.time_zone;
+            ```
+            END
+		end
+    end
+    category do
+        id 'Homebrew'
+        entry do
+            name 'Stop mysql'
+            notes <<-'END'
+            ```bash
+            brew services stop mysql
+            ```
+            END
+        end
+    end
+end
