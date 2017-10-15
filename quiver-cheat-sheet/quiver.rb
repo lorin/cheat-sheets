52d40734a3d2923389ebc5c83bab443750b1c1f5
Lorin Hochstein <lhochstein@netflix.com>
2017-10-15 12:14:28 -0700

    quiver

---
+++ quiver-cheat-sheet/quiver.rb
@@ -0,0 +1,16 @@
+cheatsheet do
+    title 'quiver'
+    docset_file_name 'quiver'
+    keyword 'quiver'
+    category do
+        id ''
+        entry do
+            name 'Find note'
+            notes <<-'END'
+            ```
+            Opt Cmd F
+            ```
+            END
+        end
+    end
+end
