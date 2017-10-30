69a343f813502865785be6d8a4ef62f52ceedfa3
Lorin Hochstein <lhochstein@netflix.com>
2017-10-29 21:26:37 -0700

    scratch

---
+++ scratch-cheat-sheet/scratch.rb
@@ -0,0 +1,32 @@
+cheatsheet do
+    title 'scratch'
+    docset_file_name 'scratch'
+    keyword 'scratch'
+    category do
+        id ''
+        entry do
+            name 'Make screen big'
+            notes <<-'END'
+            Click the rectangle in the top left-hand corner
+            END
+        end
+        entry do
+            name 'Add a sprite'
+            notes <<-'END'
+            Click "New sprite", mid-left of screen
+            END
+        end
+        entry do
+            name 'Allow others access'
+            notes <<-'END'
+            Click "Share" button
+            END
+        end
+        entry do
+            name 'Modify someone else\'s project'
+            notes <<-'END'
+            Click "See inside"
+            END
+        end
+    end
+end
