6e97136785e3287fa43b2c37831d8db8c642d1e0
Lorin Hochstein <lhochstein@netflix.com>
2017-10-31 11:12:50 -0700

    .

---
+++ gdoc-cheat-sheet/gdoc.rb
@@ -0,0 +1,14 @@
+cheatsheet do
+    title 'gdoc'
+    docset_file_name 'gdoc'
+    keyword 'gdoc'
+    category do
+        id 'Google docs'
+        entry do
+            name 'Strikethrough'
+            notes <<-'END'
+            ⌘+Shift+X
+            END
+        end
+    end
+end
