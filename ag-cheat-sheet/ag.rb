3ccede59821ef1cc427ebc3b44a22cb54b329245
Lorin Hochstein <lhochstein@netflix.com>
2017-11-07 13:41:02 -0800

    ag

---
+++ ag-cheat-sheet/ag.rb
@@ -0,0 +1,16 @@
+cheatsheet do
+    title 'ag'
+    docset_file_name 'ag'
+    keyword 'ag'
+    category do
+        id ''
+        entry do
+            name 'Ignore a directory'
+            notes <<-'END'
+            ```
+            ag --ignore vendor WARN
+            ```
+            END
+        end
+    end
+end
