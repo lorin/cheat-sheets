6a890b43058c903ec6b8c3d2e2a5dfefcafa6103
Lorin Hochstein <lhochstein@netflix.com>
2017-11-17 15:00:51 -0800

    iphone

---
+++ iphone-cheat-sheet/iphone.rb
@@ -0,0 +1,26 @@
+cheatsheet do
+    title 'iphone'
+    docset_file_name 'iphone'
+    keyword 'iphone'
+    category do
+        id 'Gestures'
+        entry do
+            name 'Home'
+            notes <<-'END'
+            Swipe up from bottom edge
+            END
+        end
+        entry do
+            name 'Task switch'
+            notes <<-'END'
+            Swipe up from bottom edge and hold
+            END
+        end
+        entry do
+            name 'Control center'
+            notes <<-'END'
+            Swipe down from top-right edge
+            END
+        end
+    end
+end
