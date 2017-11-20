1002f48ea51eeabf71f4c9d0a348d3fb12f3a622
Lorin Hochstein <lhochstein@netflix.com>
2017-11-19 19:25:57 -0800

    gradle

--- gradle-cheat-sheet/gradle.rb
+++ gradle-cheat-sheet/gradle.rb
@@ -14,6 +14,29 @@ cheatsheet do
     category do
         id 'Obscure'
         entry do
+            name 'Override source sets'
+            notes <<-'END'
+            Say you wanted to put groovy code (e.g., Spock) under src/test/java
+            intead of src/test/groovy and still have it work:
+
+            ```
+            sourceSets {
+                test {
+                    java {
+                        srcDirs = ['src/test/java']
+                    }
+                    groovy {
+                        srcDirs = ['src/test/java']
+                    }
+                    resources {
+                        srcDirs = ['src/test/resources']
+                    }
+                }
+            }
+            ```
+            END
+        end
+        entry do
             name 'Include class files'
             notes <<-'END'
             Assuming they are in the "vendor" directory:
