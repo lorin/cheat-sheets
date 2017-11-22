f8e1757cd2a4e930e24e61f44cf7b73cd2a5b740
Lorin Hochstein <lhochstein@netflix.com>
2017-11-21 20:24:15 -0800

    spacemacs

---
+++ spacemacs-cheat-sheet/spacemacs.rb
@@ -0,0 +1,41 @@
+cheatsheet do
+    title 'spacemacs'
+    docset_file_name 'spacemacs'
+    keyword 'spacemacs'
+    category do
+        id ''
+        entry do
+            name 'See also'
+            notes <<-'END'
+            The clojure cheat sheet has cloure spacemacs stuff
+            END
+        end
+    end
+    category do
+        id 'Configuring'
+        entry do
+            name 'Open config file'
+            notes <<-'END'
+            ```
+            SPC f e d
+            
+            ```
+            END
+        end
+        entry do
+            name 'Reload .spacemacs'
+            notes <<-'END'
+            ```
+            SPC f e R
+            ```
+            END
+        end
+        entry do
+            name 'Links'
+            notes <<-'END'
+            * [Quick start](https://github.com/syl20bnr/spacemacs/blob/master/doc/QUICK_START.org)
+            * [Configuring Spacemacs: A Tutorial](http://thume.ca/howto/2015/03/07/configuring-spacemacs-a-tutorial/)
+            END
+        end
+    end
+end
