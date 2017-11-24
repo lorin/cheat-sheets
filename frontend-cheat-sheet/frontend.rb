89561b588ca20183ed0a6423ac30e46be4f4aa07
Lorin Hochstein <lhochstein@netflix.com>
2017-11-23 19:47:47 -0800

    fix typo

--- frontend-cheat-sheet/frontend.rb
+++ frontend-cheat-sheet/frontend.rb
@@ -24,7 +24,7 @@ cheatsheet do
             name 'Install the required stuff'
             notes <<-'END'
             ```
-            yarn add react react-dom ewbpack webpack-dev-server path html-webpack-plugin
+            yarn add react react-dom webpack webpack-dev-server path html-webpack-plugin
             yarn add babel-loader babel-core babel-preset-env babel-preset-react css-loader --dev
             ```
             END
