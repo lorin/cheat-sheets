9992a0acf5d2ee2eea8f872c918db0d44e426666
Lorin Hochstein <lhochstein@netflix.com>
2017-11-26 11:39:22 -0800

    gradle

--- gradle-cheat-sheet/gradle.rb
+++ gradle-cheat-sheet/gradle.rb
@@ -12,6 +12,26 @@ cheatsheet do
         end
     end
     category do
+        id 'Nebula stuff'
+        entry do
+            name 'Override recommended dependency version'
+            notes <<-'END'
+            ```groovy
+            configurations.all {
+
+                resolutionStrategy {
+                    force "org.flywaydb:flyway-core:4.2.0"
+                }
+            }
+            ```
+
+            See [nebula-dependency-recommender-plugin][1] docs.
+
+            [1]: https://github.com/nebula-plugins/nebula-dependency-recommender-plugin#51-forced-dependencies
+            END
+        end
+    end
+    category do
         id 'Obscure'
         entry do
             name 'Override source sets'
