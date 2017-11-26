fdd031b0df769f4e69e3f90321f43c0e6a4e3e6f
Lorin Hochstein <lhochstein@netflix.com>
2017-11-26 15:21:16 -0800

    jooq

--- jooq-cheat-sheet/jooq.rb
+++ jooq-cheat-sheet/jooq.rb
@@ -3,6 +3,62 @@ cheatsheet do
     docset_file_name 'jooq'
     keyword 'jooq'
     category do
-        id ''
+        id 'Code generation'
+        entry do
+            name 'Gradle setup'
+            notes <<-'END'
+            See <https://github.com/etiennestuder/gradle-jooq-plugin> for mroe details
+
+            Assumes database is named "foo", postgres as backend
+
+            ```gradle
+            buildscript {
+                ext.jooq_version = '3.10.1'
+
+                dependencies {
+                    classpath 'nu.studer:gradle-jooq-plugin:2.0.9'
+                }
+            }
+
+            dependencies {
+                compile "org.postgresql:postgresql"
+                compile "org.jooq:jooq"
+
+                jooqRuntime "org.postgresql:postgresql"
+            }
+
+            jooq {
+                foo(sourceSets.main) {
+                    jdbc {
+                        url = project.property('jooq.url')
+                        user = project.property('jooq.user')
+                        // I usually do this locally against Postgres.app, so no password here
+                    }
+                    generator {
+                        database {
+                            name = "org.jooq.util.postgres.PostgresDatabase"
+                            inputSchema = 'public'
+                        }
+
+                        target {
+                            packageName = 'com.example.foo.jooq'
+                            directory = 'generated/src/main/java'
+                        }
+                    }
+
+                }
+            }
+            ```
+            END
+        end
+        entry do
+            name 'Executing the task'
+            notes <<-'END'
+            ```
+            JDBC="jdbc:postgresql://localhost:5432/foo"
+            ./gradlew generateIncompleteJooqSchemaSource -Pjooq.url=$(JDBC) -Pjooq.user=$$USER
+            ```
+            END
+        end
     end
 end
