1dc69ceeb1e28ce9130cc271c3203bfc61ffe5cb
Lorin Hochstein <lhochstein@netflix.com>
2017-11-26 17:14:30 -0800

    kotlin

--- kotlin-cheat-sheet/kotlin.rb
+++ kotlin-cheat-sheet/kotlin.rb
@@ -38,4 +38,34 @@ cheatsheet do
             END
         end
     end
+    category do
+        id 'Annotations'
+        entry do
+            name 'Annotate constructor'
+            notes <<-'END'
+            Here's an example from Spring where the constructor is annotated with `@Autowired`
+
+            ```kotlin
+            package org.example.foo
+
+            import org.example.foo.jooq.Tables.WIDGETS
+            import org.example.foo.service.WidgetService
+            import org.springframework.beans.factory.annotation.Autowired
+            import org.springframework.web.bind.annotation.RequestMapping
+            import org.springframework.web.bind.annotation.RestController
+
+            @RestController
+            class WidgetController @Autowired constructor (private val widgetService : WidgetService) {
+                @RequestMapping("api/widgets")
+                fun widgets() : List<Map<String, String>> {
+                    return widgetService.all.map{
+                        hashMapOf("name" to it.get(WIDGETS.NAME),
+                                  "description" to it.get(WIDGETS.DESCRIPTION))
+                    }
+                }
+            }
+            ```
+            END
+        end
+    end
 end
