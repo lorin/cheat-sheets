925c1c166119c28887bc9c1ff3abd4f9c472de29
Lorin Hochstein <lhochstein@netflix.com>
2017-12-12 23:17:47 -0800

    omnifocus

---
+++ omnifocus-cheat-sheet/omnifocus.rb
@@ -0,0 +1,17 @@
+cheatsheet do
+    title 'omnifocus'
+    docset_file_name 'omnifocus'
+    keyword 'omnifocus'
+    category do
+        id 'Keyboard shortcuts'
+        entry do
+            name 'Move stuff up and down'
+            notes <<-'END'
+            ```
+            ^ ⌘ Up
+            ^ ⌘ Down
+            ```
+            END
+        end
+    end
+end
