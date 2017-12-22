406cea53bbb25f6b21b4091f2280d9b8fcfee0d4
Lorin Hochstein <lhochstein@netflix.com>
2017-12-21 16:37:36 -0800

    redux

---
+++ redux-cheat-sheet/redux.rb
@@ -0,0 +1,19 @@
+cheatsheet do
+    title 'redux'
+    docset_file_name 'redux'
+    keyword 'redux'
+    category do
+        id 'Basic example'
+        entry do
+            name 'Reducer'
+            notes <<-'END'
+            ```tsx
+            // reducers/index.tsx
+            // Taken from reactjs/redux vanilla counter example
+            
+            ```
+            END
+        end
+
+    end
+end
