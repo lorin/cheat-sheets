c1eada82d29c0aab926f3cf9335e878b5f0e783c
Lorin Hochstein <lhochstein@netflix.com>
2018-03-13 09:10:19 -0700

    slack

---
+++ slack-cheat-sheet/slack.rb
@@ -0,0 +1,27 @@
+cheatsheet do
+    title 'slack'
+    docset_file_name 'slack'
+    keyword 'slack'
+    category do
+        id 'Keyboard shortcuts'
+        entry do
+            name 'Jump to a channel with activity'
+            notes <<-'END'
+            ```
+            CMD K
+            ```
+            END
+        end
+    end
+    category do
+        id 'Commands'
+        entry do
+            name 'Mute the channel'
+            notes <<-'END'
+            ```
+            /mute
+            ```
+            END
+        end
+    end
+end
