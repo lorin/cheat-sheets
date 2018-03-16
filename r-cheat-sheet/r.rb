5cb8ceaa2afa5ab7389937187ff1fed9ad96e8a9
Lorin Hochstein <lhochstein@netflix.com>
2018-03-16 10:13:59 -0700

    r

---
+++ r-cheat-sheet/r.rb
@@ -0,0 +1,16 @@
+cheatsheet do
+    title 'r'
+    docset_file_name 'r'
+    keyword 'r'
+    category do
+        id ''
+        entry do
+            name ''
+            notes <<-'END'
+            ```
+            
+            ```
+            END
+        end
+    end
+end
