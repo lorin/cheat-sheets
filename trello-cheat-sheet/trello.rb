14085e63aa30c9ae80c983e711c3c6a62a9c82fb
Lorin Hochstein <lhochstein@netflix.com>
2018-05-07 10:02:35 -0700

    trello

---
+++ trello-cheat-sheet/trello.rb
@@ -0,0 +1,16 @@
+cheatsheet do
+    title 'trello'
+    docset_file_name 'trello'
+    keyword 'trello'
+    category do
+        id 'Keyboard shortcuts'
+        entry do
+            name 'My tickets'
+            notes <<-'END'
+            ```
+            Q
+            ```
+            END
+        end
+    end
+end
