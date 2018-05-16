960aac9d0c3230f28d962435a09fae5101e6a351
Lorin Hochstein <lhochstein@netflix.com>
2018-05-16 09:53:13 -0700

    trello

--- trello-cheat-sheet/trello.rb
+++ trello-cheat-sheet/trello.rb
@@ -12,5 +12,45 @@ cheatsheet do
             ```
             END
         end
+        entry do
+            name 'Filter'
+            notes <<-'END'
+            ```
+            F
+            ```
+            END
+        end
+        entry do
+            name 'Clear filters'
+            notes <<-'END'
+            ```
+            X
+            ```
+            END
+        end
+        entry do
+            name 'Toggle menu'
+            notes <<-'END'
+            ```
+            W
+            ```
+            END
+        end
+        entry do
+            name 'Quick edit'
+            notes <<-'END'
+            ```
+            E
+            ```
+            END
+        end
+        entry do
+            name 'Search'
+            notes <<-'END'
+            ```
+            /
+            ```
+            END
+        end
     end
 end
