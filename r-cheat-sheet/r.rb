3aa01c0fa1841b1b7e1084648d932291bc061e91
Lorin Hochstein <lhochstein@netflix.com>
2018-05-20 23:05:16 -0700

    r

--- r-cheat-sheet/r.rb
+++ r-cheat-sheet/r.rb
@@ -3,12 +3,15 @@ cheatsheet do
     docset_file_name 'r'
     keyword 'r'
     category do
-        id ''
+        id 'ggplot'
         entry do
-            name ''
+            name 'Simple line plot'
             notes <<-'END'
-            ```
-            
+            ```r
+            library(ggplot2)
+            p_grid = seq(from=0, to=1, length.out=20)
+            likelihood = dbinom(6, size=9, prob=p_grid)
+            ggplot(mapping=aes(x=p_grid, y=likelihood))+geom_line()
             ```
             END
         end
