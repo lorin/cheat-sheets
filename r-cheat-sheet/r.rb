1181dd75b702eeec71e1b281a4929dd677c8928a
Lorin Hochstein <lhochstein@netflix.com>
2018-05-21 22:36:41 -0700

    r

--- r-cheat-sheet/r.rb
+++ r-cheat-sheet/r.rb
@@ -3,6 +3,33 @@ cheatsheet do
     docset_file_name 'r'
     keyword 'r'
     category do
+        id 'R notebook'
+        entry do
+            name 'Insert code chunk'
+            notes <<-'END'
+            ```
+            Cmd+Option+I
+            ```
+            END
+        end
+        entry do
+            name 'Evaluate code chunk'
+            notes <<-'END'
+            You need to put `{r}` after the code fence.
+
+            To evaluate: Cmd+Shift+Enter
+            END
+        end
+        entry do
+            name 'HTML preview'
+            notes <<-'END'
+            ```
+            Cmd+Shift+K
+            ```
+            END
+        end
+    end
+    category do
         id 'ggplot'
         entry do
             name 'Simple line plot'
