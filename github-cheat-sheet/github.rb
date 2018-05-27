9ba8c0e5dfdf11ecde5a61e19ea9643be14ae29a
Lorin Hochstein <lhochstein@netflix.com>
2018-05-26 18:20:08 -0700

    github

---
+++ github-cheat-sheet/github.rb
@@ -0,0 +1,16 @@
+cheatsheet do
+    title 'github'
+    docset_file_name 'github'
+    keyword 'github'
+    category do
+        id 'Keyboard shorcuts'
+        entry do
+            name 'Incremental search for filename'
+            notes <<-'END'
+            ```
+            t
+            ```
+            END
+        end
+    end
+end
