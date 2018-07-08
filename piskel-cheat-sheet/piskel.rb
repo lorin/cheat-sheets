9c06392a660d80c3b840988294e873def42c249b
Lorin Hochstein <lhochstein@netflix.com>
2018-07-08 15:51:53 -0700

    piskel

---
+++ piskel-cheat-sheet/piskel.rb
@@ -0,0 +1,31 @@
+cheatsheet do
+    title 'piskel'
+    docset_file_name 'piskel'
+    keyword 'piskel'
+    category do
+        id ''
+        entry do
+            name 'Site'
+            notes <<-'END'
+            <http://www.piskelapp.com>
+            END
+        end
+        entry do
+            name 'Set up for a 16x16 PNG (Minecraft item)'
+            notes <<-'END'
+            1. Log into [Piskel](http://www.piskelapp.com)
+            1. Click "Create Sprite" at the top
+            1. Click the Resize icon on the right (under the gear)
+            1. Choose 16 px for width and height, and click "Resize"
+            END
+        end
+        entry do
+            name 'Export to png'
+            notes <<-'END'
+            1. Click the Export icon on the right (frame of mountain)
+            1. Click PNG tab
+            1. Click "Download"
+            END
+        end
+    end
+end
