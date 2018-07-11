9aa84abe58df212cdfc38e86565f937ba19c5113
Lorin Hochstein <lhochstein@netflix.com>
2018-07-10 22:17:48 -0700

    omnigraffle

---
+++ omnigraffle-cheat-sheet/omnigraffle.rb
@@ -0,0 +1,35 @@
+cheatsheet do
+    title 'omnigraffle'
+    docset_file_name 'omnigraffle'
+    keyword 'omnigraffle'
+    category do
+        id 'Text'
+        entry do
+            name 'Left justify'
+            notes <<-'END'
+            * ^⌘{
+            * Format -> Text -> Align Left
+            END
+        end
+        entry do
+            name 'Change font color'
+            notes <<-'END'
+            * Select text object
+            * Click "Inspect" at top-right
+            * Choose "T" box (second option)
+            * Under "Font", choose color
+            END
+        end
+    end
+
+    category do
+        id 'Stencils'
+        entry do
+            name 'Stick figure'
+            notes <<-'END'
+            1. Inspectors -> Stencils
+            2. Software -> UML-Use Case
+            END
+        end
+    end
+end
