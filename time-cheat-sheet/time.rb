4115791107372410b49b13c350cbf09daf9874a7
Lorin Hochstein <lhochstein@netflix.com>
2018-08-03 08:46:30 -0700

    time

--- time-cheat-sheet/time.rb
+++ time-cheat-sheet/time.rb
@@ -148,4 +148,33 @@ cheatsheet do
             END
         end
     end
+    category do
+        id 'ISO 8601 durations'
+        entry do
+            name 'General syntax'
+            notes <<-'END'
+            ```
+            P[n]Y[n]M[n]DT[n]H[n]M[n]S
+            ```
+
+            * P is the duration designator (for period) placed at the start of the duration representation.
+            * Y is the year designator that follows the value for the number of years.
+            * M is the month designator that follows the value for the number of months.
+            * W is the week designator that follows the value for the number of weeks.
+            * D is the day designator that follows the value for the number of days.
+            * T is the time designator that precedes the time components of the representation.
+            * H is the hour designator that follows the value for the number of hours.
+            * M is the minute designator that follows the value for the number of minutes.
+            * S is the second designator that follows the value for the number of seconds.
+
+            Taken from Wikipedia ISO 8601 entry.
+            END
+        end
+        entry do
+            name 'Example'
+            notes <<-'END'
+            `PT20m` - 20 minute duration
+            END
+        end
+    end
 end
