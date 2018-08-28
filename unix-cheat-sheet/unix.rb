d378a96a23b86c8533ac9a3b0b3bb6471cd7544e
Lorin Hochstein <lhochstein@netflix.com>
2018-08-28 15:47:26 -0700

    unix

---
+++ unix-cheat-sheet/unix.rb
@@ -0,0 +1,16 @@
+cheatsheet do
+    title 'unix'
+    docset_file_name 'unix'
+    keyword 'unix'
+    category do
+        id ''
+        entry do
+            name 'Generate random instance id'
+            notes <<-'END'
+            ```
+            
+            ```
+            END
+        end
+    end
+end
