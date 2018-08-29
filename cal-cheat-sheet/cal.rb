6f794d4bc09a7b528b5ae3bc1703611793da8470
Lorin Hochstein <lhochstein@netflix.com>
2018-08-29 08:08:36 -0700

    google cal

---
+++ cal-cheat-sheet/cal.rb
@@ -0,0 +1,18 @@
+cheatsheet do
+    title 'cal'
+    docset_file_name 'cal'
+    keyword 'cal'
+    category do
+        id 'Google calendar'
+        entry do
+            name 'Change event owner'
+            notes <<-'END'
+            1. Click the event
+            2. Click the three vertical dots in the top menu
+            3. Choose "Change Owner"
+
+            Doc: <https://support.google.com/calendar/answer/78739?co=GENIE.Platform%3DDesktop&hl=en>
+            END
+        end
+    end
+end
