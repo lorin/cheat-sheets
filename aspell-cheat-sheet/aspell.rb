f29cdeabe7bb61423b2377d2673b99ad793f0c0f
Lorin Hochstein <lhochstein@netflix.com>
2018-09-11 16:04:25 -0700

    aspell

---
+++ aspell-cheat-sheet/aspell.rb
@@ -0,0 +1,16 @@
+cheatsheet do
+    title 'aspell'
+    docset_file_name 'aspell'
+    keyword 'aspell'
+    category do
+        id ''
+        entry do
+            name 'check a doc'
+            notes <<-'END'
+            ```
+            aspell check file.txt
+            ```
+            END
+        end
+    end
+end
