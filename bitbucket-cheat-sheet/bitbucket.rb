b4abf83081ba0cd911cf8ca746c64322df90662f
Lorin Hochstein <lhochstein@netflix.com>
2018-10-09 14:36:39 -0700

    bitbucket

---
+++ bitbucket-cheat-sheet/bitbucket.rb
@@ -0,0 +1,14 @@
+cheatsheet do
+    title 'bitbucket'
+    docset_file_name 'bitbucket'
+    keyword 'bitbucket'
+    category do
+        id 'Code'
+        entry do
+            name 'View the current file at a commit'
+            notes <<-'END'
+            Click the History drop down and click the top one.
+            END
+        end
+    end
+end
