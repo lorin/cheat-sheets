bfb3711c9ae9b3da6f205368d3f010456cdfdd02
Lorin Hochstein <lhochstein@netflix.com>
2018-10-28 11:20:09 -0700

    pathfinder

---
+++ pathfinder-cheat-sheet/pathfinder.rb
@@ -0,0 +1,18 @@
+cheatsheet do
+    title 'pathfinder'
+    docset_file_name 'pathfinder'
+    keyword 'pathfinder'
+    category do
+        id 'Customize view'
+        entry do
+            name 'Add panes'
+            notes <<-'END'
+            ```
+            Window -> Modules
+            ```
+
+            Then drag and drop the modules you want.
+            END
+        end
+    end
+end
