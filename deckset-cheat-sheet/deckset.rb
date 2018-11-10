1586f1bd379f0ffd5b5773db9f1ccb0d2f0e68dc
Lorin Hochstein <lhochstein@netflix.com>
2018-11-10 15:57:34 -0800

    checkpoint

--- deckset-cheat-sheet/deckset.rb
+++ deckset-cheat-sheet/deckset.rb
@@ -5,6 +5,22 @@ cheatsheet do
     category do
         id ''
         entry do
+            name 'One line'
+            notes <<-'END'
+            ```
+            # [fit] Text fits!
+            ```
+            END
+        end
+        entry do
+            name 'Image (full slide)'
+            notes <<-'END'
+            ```
+            ![fit](chaos-monkey.jpg)
+            ```
+            END
+        end
+        entry do
             name 'Per-slide Footers'
             notes <<-'END'
             ```
@@ -29,6 +45,25 @@ cheatsheet do
                 com.decksetapp.Theme	   5
                 com.decksetapp.Thumbnail	22505
             ```
+            END
+        end
+    end
+    category do
+        id 'Other'
+        entry do
+            name 'Smart copy and paste'
+            notes <<-'END'
+
+            To copy a slide from Deckset to another document, just ⌘+C it, then
+            ⌘+V it into your editor (it will paste the _Markdown_) or into any
+            application that handles PDFs (it will paste the slide as _PDF_).
+
+            END
+        end
+        entry do
+            name 'Emoji'
+            notes <<-'END'
+            See [Emoji docs](https://docs.decksetapp.com/English.lproj/Formatting/09-emojis.html)
             END
         end
     end
