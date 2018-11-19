da2d3c13732056187ecb5058acd3e39627e05991
Lorin Hochstein <lhochstein@netflix.com>
2018-11-19 12:05:05 -0800

    checkpoint

--- gmail-cheat-sheet/gmail.rb
+++ gmail-cheat-sheet/gmail.rb
@@ -3,6 +3,19 @@ cheatsheet do
     docset_file_name 'gmail'
     keyword 'gmail'
     category do
+        id 'Filtering'
+        entry do
+            name 'On a certain date'
+            notes <<-'END'
+            ```
+            label:my-label after:2018/11/12 before:2018/11/13  
+            ```
+
+            Above will match emails on Nov. 12, 2018.
+            END
+        end
+    end
+    category do
         id ''
         entry do
             name 'bcc'
