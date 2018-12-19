dd1f9f02e83dc573249cffc4e2cd2131e64f938c
Lorin Hochstein <lhochstein@netflix.com>
2018-12-19 15:55:39 -0800

    update

--- slack-cheat-sheet/slack.rb
+++ slack-cheat-sheet/slack.rb
@@ -35,9 +35,83 @@ cheatsheet do
     category do
         id 'API'
         entry do
-            name 'channels.history API'
+            name 'Getting an auth token for reading public history'
             notes <<-'END'
-            <https://api.slack.com/methods/channels.history>
+            You need an app with an associated bot.
+
+            1. Go to <https://api.slack.com/apps>
+            1. Click "Create New App" 
+            1. Click "Add features and functionality"
+            1. Click "Bots"
+            1. Give your bot a name, click "Save Changes"
+            1. Click "Basic Information" on the left.
+            1. Click "Add features and functionality".
+            1. Click "Permissions".
+            1. Under "Scopes", choose "channels:history" and "channels:read"
+            1. Click "Install app"
+
+            That will create the bot in your Slack.
+
+            Use the OAuth Access Token (I think the oauth acceess token is for reading, bot user for writing).
+
+            END
+        end
+        entry do
+            name 'Get posts: channels.history'
+            notes <<-'END'
+            Documented at: <https://api.slack.com/methods/channels.history>
+
+            ```bash
+            http -b https://slack.com/api/channels.history token==$SLACK_TOKEN channel==CEY6WLNSY
+            ```
+
+            ```json
+            {
+                "ok": true,
+                "has_more": false,
+                "messages": [
+                    {
+                        "client_msg_id": "5bff938c-4b3a-4db4-9e1b-0792ef3606ed",
+                        "text": "Foo the bar!",
+                        "ts": "1545259760.000400",
+                        "type": "message",
+                        "user": "UEYHRCKRB"
+                    },
+                    {
+                        "subtype": "channel_join",
+                        "text": "<@UEYHRCKRB> has joined the channel",
+                        "ts": "1545258127.000200",
+                        "type": "message",
+                        "user": "UEYHRCKRB"
+                    }
+                ]
+            }
+            ```
+            END
+        end
+        entry do
+            name 'Get channel names: conversations.list'
+            notes <<-'END'
+            Documented at: <https://api.slack.com/methods/conversations.list>
+
+            ```bash
+            http https://slack.com/api/conversations.list token==$SLACK_TOKEN | jq '.channels[] | {id: .id, name: .name}'
+            ```
+
+            ```json
+            {
+              "id": "CEY6WLNSY",
+              "name": "random"
+            }
+            {
+              "id": "CEYBVB42E",
+              "name": "general"
+            }
+            {
+              "id": "CEYBVB85Q",
+              "name": "stuff"
+            }
+            ```
             END
         end
     end
