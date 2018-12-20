0efd55fe5c10f6251914a9a234b8e377ea11adf7
Lorin Hochstein <lhochstein@netflix.com>
2018-12-20 14:19:47 -0800

    checkpoint

--- requests-cheat-sheet/requests.rb
+++ requests-cheat-sheet/requests.rb
@@ -3,6 +3,20 @@ cheatsheet do
     docset_file_name 'requests'
     keyword 'requests'
     category do
-        id ''
+        id 'Query params'
+        entry do
+            name ''
+            notes <<-'END'
+            ```python
+            import requests
+
+            url = "http://www.example.com"
+            params = {"foo": "bar", "baz": "quux"}
+            r = requests.get(url, params=params)
+            r.raise_for_status()
+            result = r.json()
+            ```
+            END
+        end
     end
 end
