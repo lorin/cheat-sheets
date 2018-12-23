546ce62fd9abc7d5e6bb1d7f20df7e819bcd5d65
Lorin Hochstein <lhochstein@netflix.com>
2018-12-23 11:02:35 -0800

    checkpoint

---
+++ wordpress-cheat-sheet/wordpress.rb
@@ -0,0 +1,16 @@
+cheatsheet do
+    title 'wordpress'
+    docset_file_name 'wordpress'
+    keyword 'wordpress'
+    category do
+        id ''
+        entry do
+            name 'Show keyboard shortcuts for visual editor'
+            notes <<-'END'
+            ```
+            ^ ⌥ H
+            ```
+            END
+        end
+    end
+end
