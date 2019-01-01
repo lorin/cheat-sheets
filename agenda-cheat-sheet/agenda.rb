6bbcce1da8736c48faf59b9d047b6409ca7acd66
Lorin Hochstein <lhochstein@netflix.com>
2019-01-01 11:25:09 -0800

    checkpoint

---
+++ agenda-cheat-sheet/agenda.rb
@@ -0,0 +1,12 @@
+cheatsheet do
+    title 'agenda'
+    docset_file_name 'agenda'
+    keyword 'agenda'
+    category do
+        id 'Keyboard shortcuts'
+        entry do
+            name 'Create checkbox item'
+            command '⇪ ⌘ C'
+        end
+    end
+end
