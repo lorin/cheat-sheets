4e062aed2b3c9d5f19d7bc656da77b086ccb9547
Lorin Hochstein <lhochstein@netflix.com>
2019-01-11 15:09:15 -0800

    checkpoint

---
+++ jupyter-cheat-sheet/jupyter.rb
@@ -0,0 +1,13 @@
+cheatsheet do
+    title 'jupyter'
+    docset_file_name 'jupyter'
+    keyword 'jupyter'
+    category do
+        id 'Keyboard shortcuts'
+        entry do
+            name 'Command pallette'
+            command '⌘ ⇪ P'
+            END
+        end
+    end
+end
