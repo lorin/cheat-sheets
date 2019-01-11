39e1b9fdf005539942b979166274cd793a2a4a74
Lorin Hochstein <lhochstein@netflix.com>
2019-01-11 15:11:31 -0800

    checkpoint

--- jupyter-cheat-sheet/jupyter.rb
+++ jupyter-cheat-sheet/jupyter.rb
@@ -5,8 +5,46 @@ cheatsheet do
     category do
         id 'Keyboard shortcuts'
         entry do
+            name 'Docs'
+            notes <<-'END'
+            <https://www.dataquest.io/blog/jupyter-notebook-tips-tricks-shortcuts/>
+            END
+        end
+        entry do
             name 'Command palette'
             command '⌘ ⇪ P'
+        end
+        entry do
+            name 'Command mode'
+            notes <<-'END'
+            ```
+            ESC
+            ```
+            END
+        end
+        entry do
+            name 'Edit mode'
+            notes <<-'END'
+            ```
+            ENTER
+            ```
+            END
+        end
+        entry do
+            name 'Add cell above'
+            notes <<-'END'
+            ```
+            A
+            ```
+            END
+        end
+        entry do
+            name 'Add cell below'
+            notes <<-'END'
+            ```
+            B
+            ```
+            END
         end
     end
 end
