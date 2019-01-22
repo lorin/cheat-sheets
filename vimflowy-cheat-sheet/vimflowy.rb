eb956e98ef76fbc9452a2a048329c06dba561e21
Lorin Hochstein <lhochstein@netflix.com>
2019-01-21 19:06:10 -0800

    checkpoint

---
+++ vimflowy-cheat-sheet/vimflowy.rb
@@ -0,0 +1,12 @@
+cheatsheet do
+    title 'vimflowy'
+    docset_file_name 'vimflowy'
+    keyword 'vimflowy'
+    category do
+        id 'Keyboard shortcuts'
+        entry do
+            name 'Zoom out'
+            command '['
+        end
+    end
+end
