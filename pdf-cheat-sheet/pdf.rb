3c032a0171dc5d2f048240eb62a209b0248871ff
Lorin Hochstein <lhochstein@netflix.com>
2019-02-18 08:12:46 -0800

    checkpoint

---
+++ pdf-cheat-sheet/pdf.rb
@@ -0,0 +1,16 @@
+cheatsheet do
+    title 'pdf'
+    docset_file_name 'pdf'
+    keyword 'pdf'
+    category do
+        id 'Preview (macOS)'
+        entry do
+            name 'Change font size'
+            notes <<-'END'
+            1. View -> Show Markup Toolbar (⌘ ⇪ A)
+            2. Click the "A" in the toolbar.
+            3. Choose the font size
+            END
+        end
+    end
+end
