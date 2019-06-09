bb0e6a20f57770cff057c649df3fbee52c5e0bd9
Lorin Hochstein <lhochstein@netflix.com>
2019-06-08 18:55:06 -0700

    checkpoint

--- applescript-cheat-sheet/applescript.rb
+++ applescript-cheat-sheet/applescript.rb
@@ -7,8 +7,35 @@ cheatsheet do
         entry do
             name 'POSIX path of script file'
             notes <<-END
-            ```
+            ```applescript
             POSIX path of (path to me)
+            ```
+            END
+        end
+        entry do
+            name 'Directory manipulation'
+            notes <<-END
+            ```applescript
+            tell application "Finder"
+                set parentDir to get POSIX path of ((container of (path to me)) as text)
+                set filePath to parentDir & "slides.pdf"
+            end tell
+            ```
+            END
+        end
+        entry do
+            name 'She-bang'
+            notes <<-END
+            ```applescript
+            #!/usr/bin/osascript
+            tell application "Finder"
+                set parentDir to get POSIX path of ((container of (path to me)) as text)
+            end tell
+
+            tell application "Deckset"
+                set filePath to parentDir & "slides.pdf"
+                export document 1 to filePath as "PDF"
+            end tell
             ```
             END
         end
