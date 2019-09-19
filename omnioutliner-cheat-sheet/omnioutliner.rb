2fec5af5aa077200be18950464f77755e16a4740
Lorin Hochstein <lhochstein@netflix.com>
2019-09-19 08:47:54 -0700

    checkpoint

---
+++ omnioutliner-cheat-sheet/omnioutliner.rb
@@ -0,0 +1,30 @@
+cheatsheet do
+    title 'omnioutliner'
+    docset_file_name 'omnioutliner'
+    keyword 'omnioutliner'
+    category do
+        id 'Basics'
+        entry do
+            name 'Indent'
+            notes <<-'END'
+            ```
+            Tab
+            ```
+            END
+        end
+        entry do
+            name 'Outdent'
+            notes <<-'END'
+            ```
+            Shift-tab
+            ```
+            END
+        end
+        entry do
+            name 'Annotate'
+            notes <<-'END'
+            Click "Note" icon after hovering, appears at left
+            END
+        end
+    end
+end
