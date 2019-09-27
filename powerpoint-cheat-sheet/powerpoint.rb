7a7debe7e651433e866ead9c3087229f9902c84a
Lorin Hochstein <lhochstein@netflix.com>
2019-09-27 08:27:22 -0700

    checkpoint

---
+++ powerpoint-cheat-sheet/powerpoint.rb
@@ -0,0 +1,22 @@
+cheatsheet do
+    title 'powerpoint'
+    docset_file_name 'powerpoint'
+    keyword 'powerpoint'
+    category do
+        id ''
+        entry do
+            name 'New slides by format'
+            notes <<-'END'
+            1. Ribbon menu → Home
+            1. New slide (caret down)
+            END
+        end
+        entry do
+            name 'Change format of existing slide'
+            notes <<-'END'
+            1. Ribbon menu → Home
+            1. Next to New Slide, Layout (caret down)
+            END
+        end
+    end
+end
