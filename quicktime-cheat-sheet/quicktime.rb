5207ecb49892e4035651d8c08be0f7675a05efeb
Lorin Hochstein <lhochstein@netflix.com>
2019-11-20 11:16:57 -0800

    checkpoint

---
+++ quicktime-cheat-sheet/quicktime.rb
@@ -0,0 +1,20 @@
+cheatsheet do
+    title 'quicktime'
+    docset_file_name 'quicktime'
+    keyword 'quicktime'
+    category do
+        id 'Keyboard shortcuts'
+        entry do
+            name 'Forward'
+            command 'L'
+        end
+        entry do
+            name 'Stop'
+            command 'K'
+        end
+        entry do
+            name 'Backwards'
+            command 'J'
+        end
+    end
+end
