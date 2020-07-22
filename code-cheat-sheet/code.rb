c2bb17462010c8f5b023b153e1b2588e8cb3d796
Lorin Hochstein <lhochstein@netflix.com>
2020-07-22 12:51:40 -0700

    checkpoint

---
+++ code-cheat-sheet/code.rb
@@ -0,0 +1,14 @@
+cheatsheet do
+    title 'Visual Studio Code'
+    docset_file_name 'code'
+    keyword 'code'
+    category do
+        id 'Keyboard shortcuts'
+        entry do
+            name 'Next tab'
+            notes <<-'END'
+            ⌘ ⎇ →
+            END
+        end
+    end
+end
