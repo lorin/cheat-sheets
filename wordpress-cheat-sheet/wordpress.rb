e648b28f968f6a3fb930aafb6d645a2fa54a12ef
Lorin Hochstein <lorinh@gmail.com>
2020-08-04 22:23:30 -0700

    checkpoint

--- wordpress-cheat-sheet/wordpress.rb
+++ wordpress-cheat-sheet/wordpress.rb
@@ -3,10 +3,20 @@ cheatsheet do
     docset_file_name 'wordpress'
     keyword 'wordpress'
     category do
-        id ''
+        id 'keyboard shortcuts'
         entry do
             command '^ ⌥ H'
             name 'Show keyboard shortcuts for visual editor'
+        end
+    end
+    category do
+        id ''
+        entry do
+            name 'Edit menu'
+            notes <<-'END'
+            1. Hover over the menu item at the top-left with the blog name
+            1. Click on "Menus"
+            END
         end
     end
 end
