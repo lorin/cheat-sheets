05c74345dac74691a595821f31fd4b4aface3558
Lorin Hochstein <lorinh@gmail.com>
2020-12-06 13:37:42 -0800

    siri

--- omnifocus-cheat-sheet/omnifocus.rb
+++ omnifocus-cheat-sheet/omnifocus.rb
@@ -14,4 +14,14 @@ cheatsheet do
             END
         end
     end
+    category do
+        id 'Siri'
+        entry do
+            name 'Add a reminder or task'
+            notes <<-'END'
+            * *Add a task named foo in OmniFocus*
+            * *Remind me to foo at 5:00 PM (on Thursday) in OmniFocus*
+            END
+        end
+    end
 end
