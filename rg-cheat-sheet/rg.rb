4ff0ff2cd3f3d83d981478cc5b1178bd76ac73ef
Lorin Hochstein <lorinh@gmail.com>
2020-12-10 10:36:34 -0800

    checkpoint

---
+++ rg-cheat-sheet/rg.rb
@@ -0,0 +1,24 @@
+cheatsheet do
+    title 'ripgrep (rg)'
+    docset_file_name 'rg'
+    keyword 'rg'
+    category do
+        id 'File types'
+        entry do
+            name 'Match against a file type'
+            notes <<-'END'
+            ```
+            rg -t kotlin '@Test'
+            ```
+            END
+        end
+        entry do
+            name 'List supported file types'
+            notes <<-'END'
+            ```
+            rg --type-list
+            ```
+            END
+        end
+    end
+end
