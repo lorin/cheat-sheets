634d159626a9cfe6ec32c769131e28a070a8a0cf
Lorin Hochstein <lorinh@gmail.com>
2021-03-04 18:01:49 -0800

    checkpoint

---
+++ json-cheat-sheet/json.rb
@@ -0,0 +1,16 @@
+cheatsheet do
+    title 'json'
+    docset_file_name 'json'
+    keyword 'json'
+    category do
+        id ''
+        entry do
+            name 'greppable json command'
+            notes <<-'END'
+            ```
+            gron
+            ```
+            END
+        end
+    end
+end
