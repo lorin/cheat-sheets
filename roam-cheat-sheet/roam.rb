2a0acd69cc84dee325b00b3b313ee3dc7fc02381
Lorin Hochstein <lorinh@gmail.com>
2021-03-07 12:11:44 -0800

    checkpoint

---
+++ roam-cheat-sheet/roam.rb
@@ -0,0 +1,24 @@
+cheatsheet do
+    title 'Roam Research'
+    docset_file_name 'roam'
+    keyword 'roam'
+    category do
+        id 'Keyboard shortcuts'
+        entry do
+            name 'Create note'
+            command 'CMD U'
+        end
+        entry do
+            name 'Typing focus on new note'
+            command 'CMD Enter'
+        end
+        entry do
+            name 'Open current page in sidebar'
+            command 'CTL SHIFT O'
+        end
+        entry do
+            name 'Open link in sidebar'
+            command 'SHIFT Click'
+        end
+    end
+end
