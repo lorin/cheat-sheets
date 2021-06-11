5ea3bafe05e5efc1e6a09eb03ddcd79835307020
Lorin Hochstein <lhochstein@netflix.com>
2021-06-11 09:13:30 -0700

    checkpoint

--- graphql-cheat-sheet/graphql.rb
+++ graphql-cheat-sheet/graphql.rb
@@ -4,5 +4,58 @@ cheatsheet do
     keyword 'graphql'
     category do
         id ''
+        entry do
+            name 'Query for version info with verifications'
+            notes <<-'END'
+            ### Query
+
+            ```
+            query ($appName: String!, $statuses: [MdArtifactStatusInEnvironment!]) {
+              application(appName: $appName) {
+                name
+                environments {
+                  name
+                  state {
+                    artifacts {
+                      name
+                      type
+                      versions(statuses: $statuses) {
+                        ...detailedVersionFields
+                      }
+                    }
+                    resources {
+                      id
+                      kind
+                      displayName
+                      location {
+                        account
+                        regions
+                      }
+                    }
+                  }
+                }
+              }
+            }
+
+            fragment detailedVersionFields on MdArtifactVersionInEnvironment {
+                version
+                status
+                verifications {
+                  id
+                  status
+                }
+              }
+            ```
+
+            ###  Query variables
+
+            ```json
+            {
+              "appName": "myappname",
+              "statuses": ["CURRENT"]
+            }
+            ```
+            END
+        end
     end
 end
