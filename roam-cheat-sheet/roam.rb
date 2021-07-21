33498cc26c776686f04d1d2720fab6c4adafc5bc
Lorin Hochstein <lhochstein@netflix.com>
2021-07-21 11:34:20 -0700

    checkpoint

--- roam-cheat-sheet/roam.rb
+++ roam-cheat-sheet/roam.rb
@@ -5,6 +5,10 @@ cheatsheet do
     category do
         id 'Keyboard shortcuts'
         entry do
+            name 'Go to daily notes'
+            command 'CTRL SHIFT D'
+        end
+        entry do
             name 'Create note'
             command 'CMD U'
         end
@@ -19,6 +23,27 @@ cheatsheet do
         entry do
             name 'Open link in sidebar'
             command 'SHIFT Click'
+        end
+    end
+    category do
+        id 'Functionality'
+        entry do
+            name 'Reference a block'
+            notes <<-'END'
+            Use ((double parens))
+
+            It will let you choose.
+            END
+        end
+        entry do
+            name 'Calculator'
+            notes <<-'END'
+            ```
+            {{calc: 4*5}}
+            ```
+
+            You can reference blocks in your calcs as well.
+            END
         end
     end
 end
