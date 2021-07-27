eba4af96d1671bcd3d04ba47367791dda6e049fc
Lorin Hochstein <lhochstein@netflix.com>
2021-07-27 14:02:07 -0700

    checkpoint

---
+++ yq-cheat-sheet/yq.rb
@@ -0,0 +1,16 @@
+cheatsheet do
+    title 'yq'
+    docset_file_name 'yq'
+    keyword 'yq'
+    category do
+        id 'Basics'
+        entry do
+            name 'Syntax highlight from stdin'
+            notes <<-'END'
+            ```bash
+            yq e . -
+            ```
+            END
+        end
+    end
+end
