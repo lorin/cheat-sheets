369bb1dea28209e5f9f9893e59432cedb9c6259c
Lorin Hochstein <lhochstein@netflix.com>
2021-08-04 15:12:56 -0700

    checkpoint

--- spectator-cheat-sheet/spectator.rb
+++ spectator-cheat-sheet/spectator.rb
@@ -4,5 +4,27 @@ cheatsheet do
     keyword 'spectator'
     category do
         id ''
+        entry do
+            name 'Basic counter'
+            notes <<-'END'
+            ```kotlin
+            import com.netflix.spectator.api.Registry
+            class MyClass(
+                private val spectator: Registry
+            ) {
+                private val counterId = spectator.createId("my.metric.name")
+
+                fun foo() {
+                    spectator.counter(
+                        counterId.withTags("action", "push", "color", "blue")
+                      ).increment()
+
+                }
+
+            }
+            
+            ```
+            END
+        end
     end
 end
