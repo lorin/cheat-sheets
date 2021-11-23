e62188c7480483ba66240875efa8e96372971dfc
Lorin Hochstein <lhochstein@netflix.com>
2021-11-22 19:33:53 -0800

    checkpoint

--- fsharp-cheat-sheet/fsharp.rb
+++ fsharp-cheat-sheet/fsharp.rb
@@ -4,5 +4,20 @@ cheatsheet do
     keyword 'fsharp'
     category do
         id ''
+        entry do
+            name 'Scripting'
+            notes <<-'END'
+            ```fsharp
+            #!/usr/bin/env dotnet fsi
+
+            let getOddSquares xs =
+                xs
+                |> List.filter (fun x -> x % 2 <> 0)
+                |> List.map (fun x -> x * x)
+
+            printfn "%A" (getOddSquares [1..10])
+            ```
+            END
+        end
     end
 end
