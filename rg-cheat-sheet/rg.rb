9571aaf272c4f24fb73755ded180f0f1549c0b78
Lorin Hochstein <lhochstein@netflix.com>
2021-11-29 15:41:02 -0800

    checkpoint

--- rg-cheat-sheet/rg.rb
+++ rg-cheat-sheet/rg.rb
@@ -3,11 +3,23 @@ cheatsheet do
     docset_file_name 'rg'
     keyword 'rg'
     category do
+        id 'Common tasks'
+        entry do
+            name 'Only show file names'
+            notes <<-'END'
+            ```bash
+            rg --files-with-matches regular_expression
+            ```
+            END
+        end
+
+    end
+    category do
         id 'File types'
         entry do
             name 'Match against a file type (language)'
             notes <<-'END'
-            ```
+            ```bash
             rg -t kotlin '@Test'
             ```
             END
@@ -15,7 +27,7 @@ cheatsheet do
         entry do
             name 'List supported file types'
             notes <<-'END'
-            ```
+            ```bash
             rg --type-list
             ```
             END
