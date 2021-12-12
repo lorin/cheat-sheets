c25845d811367962cdfaa8a7c33a00f3268b9d43
Lorin Hochstein <lhochstein@netflix.com>
2021-12-12 10:26:19 -0800

    checkpoint

---
+++ figma-cheat-sheet/figma.rb
@@ -0,0 +1,44 @@
+cheatsheet do
+    title 'figma'
+    docset_file_name 'figma'
+    keyword 'figma'
+    category do
+        id 'Shortcuts'
+        entry do
+            name 'Panning'
+            notes <<-'END'
+            If you hold space, click dragging pans the screen
+            END
+        end
+        entry do
+            name 'Zoom to fit'
+            notes <<-'END'
+            ```
+            SHIFT 1
+            ```
+            END
+        end
+        entry do
+            name 'Zoom to 100%'
+            notes <<-'END'
+            ```
+            SHIFT 0
+            ```
+            END
+        end
+        entry do
+            name 'Move a frame to a different page'
+            notes <<-'END'
+            Right click on frame
+            END
+        end
+        entry do
+            name 'Duplicate a frame'
+            notes <<-'END'
+            ```
+            CMD D
+            ```
+            END
+        end
+    end
+end
