d2a4f880ca1720a2c7ab6405067b96ebfd1a7b0b
Lorin Hochstein <lhochstein@netflix.com>
2022-08-16 11:28:59 -0700

    checkpoint

--- jackson-cheat-sheet/jackson.rb
+++ jackson-cheat-sheet/jackson.rb
@@ -3,6 +3,22 @@ cheatsheet do
     docset_file_name 'jackson'
     keyword 'jackson'
     category do
-        id ''
+        id 'Kotlin'
+        entry do
+            name 'Missing field exception'
+            notes <<-'END'
+            If there's a missing field, Jackson throws a
+            [MissingKotlinParameterException](https://github.com/FasterXML/jackson-module-kotlin/blob/master/src/main/kotlin/com/fasterxml/jackson/module/kotlin/Exceptions.kt).
+
+            Here's an example that just prints the missing field name
+
+            ```kotlin
+            try {
+                // deserialization happens here
+            }  catch(e: MissingKotlinParameterException) {
+                println("Missing field is: ${parameter.name})
+            ```
+            END
+        end
     end
 end
