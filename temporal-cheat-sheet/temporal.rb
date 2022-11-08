09f77c05e5e97483fc1838c9ecfb3ebf4692888e
Lorin Hochstein <lhochstein@netflix.com>
2022-11-07 16:30:00 -0800

    checkpoint

---
+++ temporal-cheat-sheet/temporal.rb
@@ -0,0 +1,22 @@
+cheatsheet do
+    title 'temporal'
+    docset_file_name 'temporal'
+    keyword 'temporal'
+    category do
+        id 'UI'
+        entry do
+            name 'Query docs'
+            notes <<-'END'
+            <https://docs.temporal.io/visibility#search-attribute>
+            END
+        end
+        entry do
+            name 'List workflows with a certain name'
+            notes <<-'END'
+            ```
+            WorkflowType = "EnvironmentScheduler"
+            ```
+            END
+        end
+    end
+end
