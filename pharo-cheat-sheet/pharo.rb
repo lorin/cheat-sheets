956c5d74e0816ffd9c469e8caa9b94fc3a443038
Lorin Hochstein <lhochstein@netflix.com>
2022-11-16 15:45:26 -0800

    checkpoint

---
+++ pharo-cheat-sheet/pharo.rb
@@ -0,0 +1,14 @@
+cheatsheet do
+    title 'pharo'
+    docset_file_name 'pharo'
+    keyword 'pharo'
+    category do
+        id ''
+        entry do
+            name 'Cheat sheat'
+            notes <<-'END'
+            <http://files.pharo.org/media/pharoCheatSheet.pdf>
+            END
+        end
+    end
+end
