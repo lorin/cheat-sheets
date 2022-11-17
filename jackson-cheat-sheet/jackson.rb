2143ba73bd6ab081e3870513e707bc10af5a8b2b
Lorin Hochstein <lhochstein@netflix.com>
2022-11-17 14:08:09 -0800

    checkpoint

--- jackson-cheat-sheet/jackson.rb
+++ jackson-cheat-sheet/jackson.rb
@@ -3,6 +3,34 @@ cheatsheet do
     docset_file_name 'jackson'
     keyword 'jackson'
     category do
+        id 'Basics'
+        entry do
+            name 'Serialize'
+            notes <<-'END'
+            ```
+            val mapper = ObjectMapper()
+            val obj = ...
+            val s = mapper.writeValueAsString(obj)
+            ```
+            END
+        end
+    end
+    category do
+        id 'Odds and ends'
+        entry do
+            name 'Convert from object to map'
+            notes <<-'END'
+            ```
+            val mapper = ObjectMapper()
+            val obj = ...
+            val m = mapper.convertValue(obj, Map::class.java)
+            ```
+            END
+        end
+
+    end
+
+    category do
         id 'Kotlin'
         entry do
             name 'Missing field exception'
